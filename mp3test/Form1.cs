771bff04d5beb9791c00da06c94c878b71c4386d
melarKode <58908326+melarKode@users.noreply.github.com>
2020-02-18 09:27:47 +0530

    Timer functionality added to help play the next song. Need to work on queues

--- mp3test/Form1.cs
+++ mp3test/Form1.cs
@@ -6,39 +6,79 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace mp3test
 {
     public partial class Form1 : Form
     {
+        int _prevState;
+        System.Timers.Timer _monitorTimer;
         public Form1()
         {
             InitializeComponent();
-        }
-        string[] files, paths;
-        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
-        {
-            int i;
-            if(axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            //run a timer once every second
+            _monitorTimer = new System.Timers.Timer(1000);
+            _monitorTimer.Elapsed += (s, e) =>
             {
-                i = listBox1.SelectedIndex;
-                i = (i + 1) % (files.Length);
-                axWindowsMediaPlayer1.URL = paths[i];
-                axWindowsMediaPlayer1.playState.Equals(WMPLib.WMPPlayState.wmppsPlaying);
-                label1.Text = String.Format("\n {0}", files[i]);
-                
-            }
+                //every tick of timer, check if media playing previously has ended
+                if (_prevState == 1)
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                    //set URL to next song in the queue or default next song
+                    axWindowsMediaPlayer1.URL = @"C:\Users\prave\Music\Playlist\BMK201-Sobillu-Jaganmohini.mp3";
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    _prevState = 0;
+                }
+            };
+            _monitorTimer.Start();
+            axWindowsMediaPlayer1.PlayStateChange += (s, e) =>
+            {
+                if (e.newState == 8)//mediaEnded
+                {
+                    _prevState = 1;
+                }
+            };
+            axWindowsMediaPlayer1.URL = @"C:\Users\prave\Music\Playlist\BMK206-Bangarumurali-Neelambari.mp3";
         }
-
+        string[] files, paths, paths2, files2;
         private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             int i;
             i = listBox1.SelectedIndex;
-            axWindowsMediaPlayer1.URL = paths[i];
+            if(listBox2.SelectedIndex==-1)
+                axWindowsMediaPlayer1.URL = paths[i];
             label1.Text = String.Format("\n {0}", files[i]);
         }
-        private void button1_Click(object sender, EventArgs e)
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                listBox2.Items.Add(files[listBox1.SelectedIndex]);
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void listBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            button3.Visible = false;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
         {
             int i;
             if(openFileDialog1.ShowDialog()==System.Windows.Forms.DialogResult.OK)
